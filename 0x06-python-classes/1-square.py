f79fe1b2e38ae70a8448408d5d09dc267180a115
mikolanny <olaniyanlanny@gmail.com>
2023-06-27 14:13:23 -0700

    [fixed]: updated classea functions

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
+"""
+class square for 0-square-py
+"""
+
 
 class Square:
-
+    """
+    class with private attribute size
+    """
     def __init__(self, size):
 
         self.__size = size
